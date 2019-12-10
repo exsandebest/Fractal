dc5187a07921acd13734d2bb143691cd5c993cd9
Daniil Bogdanov <51285775+exsandebest@users.noreply.github.com>
2019-12-10 19:44:53 +0300

    complex ~

---
+++ complex.h
@@ -0,0 +1,32 @@
+#ifndef COMPLEX_H
+#define COMPLEX_H
+#include <math.h>
+
+class complex {
+public:
+    double r,i;
+    complex(double re, double im){
+        r = re;
+        i = im;
+    }
+    complex(complex & c){
+        r = c.r;
+        i = c.i;
+    }
+    ~complex();
+
+    complex operator+(complex & c){
+        return *new complex(this->r + c.r, this->i + c.i);
+    }
+    complex operator-(complex & c){
+        return *new complex(this->r - c.r, this->i - c.i);
+    }
+    complex operator*(complex & c){
+        return *new complex(this -> r * c.r - this -> i * c.i, this -> i * c.r + this -> r * c.i);
+    }
+    double v(){
+        return sqrt(r*r+i*i);
+    }
+};
+
+#endif // COMPLEX_H
